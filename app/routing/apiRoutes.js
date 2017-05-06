9f5c98effb9c6101e51943ecbec9a2222052c3b2
Jieun Rim <rim.jieun@gmail.com>
2017-05-06 19:14:10 -0400

    added compatibility algorithm

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,39 +2,72 @@
 // POST route /api/friends, used to handle incoming survey results
 // and also to handle compatibility logic
 
-var users = require("../data/friends");
+var fs = require("fs");
+
+var json = fs.readFileSync("app/data/friends.json");
+var friendsArr = JSON.parse(json); //array of friends data
 
 module.exports = function(app) {
+
+
   app.get("/api/friends", function(req, res) {
-    res.json(users);
+    res.json(friends);
   });
 
-  app.post("/api/friends", function(req, res) {
-    users.push(req.body);
+  app.post("/api/new/friend", function(req, res) {
+    
+    var newFriend = {
+      name: req.body.name,
+      photoURL: req.body.photoURL,
+      scores: req.body.scores.map(Number)
+    };
+
+    console.log(bestMatch(newFriend));
+
+    friendsArr.push(newFriend);
+    var data = JSON.stringify(friendsArr, null, 2);
+    fs.writeFile("app/data/friends.json", data);
+
+    res.json(req.body);
+
   });
 
-  app.get("/api/bestmatch", function(req, res) {
-    res.json(bestMatch());
-  });
+  // app.get("/api/bestmatch", function(req, res) {
+  //   res.json(bestMatch());
+  // });
 };
 
-function bestMatch() {
-
-  var newUser = users.pop();
-  var bestMatch;
+function bestMatch(friend) {
   var maxDiff = 40;
-  
-  for (i = 0; i < users.length; i++) {
+  var bestMatch = [];
+
+  for (i = 0; i < friendsArr.length; i++) {
 
     var totalDiff = 0;
 
-    for (j = 0; j < users[i].scores.length; j++) {
-      var diff = Math.abs(users[i].scores[j] - newUser.scores[j]);
-      totalDiff += diff;
+    for (j = 0; j < friend.scores.length; j++) {
+      var scoreDiff = Math.abs(friendsArr[i].scores[j] - friend.scores[j]);
+      totalDiff += scoreDiff;
     }
 
-    if (totalDiff < maxDiff) {
-      bestMatch = users[i];
+
+    if (totalDiff === maxDiff) {
+      var match = {
+        name: friendsArr[i].name,
+        photoURL: friendsArr[i].photoURL
+      };
+
+      bestMatch.push(match);
+
+    } else if (totalDiff < maxDiff) {
+
+      bestMatch = [
+        {
+          name: friendsArr[i].name,
+          photoURL: friendsArr[i].photoURL
+        }
+      ];
+      
       maxDiff = totalDiff;
     }
   }
